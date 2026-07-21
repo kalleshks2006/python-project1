730bb88c2e300b410d6a5ef65f6d6d7980c1670e
kalleshks2006 <kalleshk736@gmail.com>
2026-07-21 18:46:55 +0530

    my second python program

---
+++ basics.py
@@ -0,0 +1,14 @@
+name=input("enter your name:")
+age=int(input("enter your age:"))
+next_age=age+1
+print("\n student Details")
+print("Name:" + name)
+print("Age:" + str(age))
+print("Next Age:" + str(next_age))
+a=10
+b=5
+print("\n ----Arithmetic operations----")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
