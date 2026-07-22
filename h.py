c2f912aac000d40e22853c945c48071313608ac0
kalleshks2006 <kalleshk736@gmail.com>
2026-07-22 18:10:07 +0530

    python turtle p3

---
+++ h.py
@@ -0,0 +1,21 @@
+import turtle
+t=turtle.Turtle()
+
+
+for i in range(4):
+    t.forward(200)
+    t.right(90)
+    t.right(90)
+    t.left(90)
+    t.right(90)
+turtle.done  
+
+import turtle
+t=turtle.Turtle()
+
+while i in range(6):
+    t.forward(100)
+    t.right(90)
+    t.circle(50)
+    t.color(red)
+turtle.done     
